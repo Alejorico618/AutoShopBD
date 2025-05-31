4971c0e99483f2fcd3b0b60994547310d49e7d9f
Alejorico618 <d.alejandrorico2@gmail.com>
2025-05-31 11:38:46 -0500

    creacion Clientes y partes .cs

--- Models/Clientes.cs
+++ Models/Clientes.cs
@@ -1,6 +1,24 @@
-﻿namespace AutoShopBD.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoShopBD.Models
 {
     public class Clientes
     {
+        [Key]
+        public int IdCliente { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string Telefono { get; set; }
+
+        [EmailAddress]
+        public string Correo { get; set; }
+
+        public string Direccion { get; set; }
+
+        public ICollection<Vehiculos> Vehiculos { get; set; }
     }
 }
