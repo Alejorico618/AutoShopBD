5d61e56596539e29ed4641746a34fec170526472
Alejorico618 <d.alejandrorico2@gmail.com>
2025-05-31 11:59:44 -0500

    Creacion carpeta data, ajuste de coneccion .json y program.cs

---
+++ Data/AutoShopBDContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AutoShopBD.Models;
+
+namespace AutoShopBD.Data
+{
+    public class AutoShopBDContext : DbContext
+    {
+        public AutoShopBDContext(DbContextOptions<AutoShopBDContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Vehiculos> Vehiculos { get; set; }
+        public DbSet<Reparaciones> Reparaciones { get; set; }
+        public DbSet<Partes> Partes { get; set; }
+        public DbSet<Citas> Citas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Citas>()
+                .HasOne(c => c.Vehiculo)
+                .WithMany()
+                .HasForeignKey(c => c.IdVehiculo)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Citas>()
+                .HasOne(c => c.Cliente)
+                .WithMany()
+                .HasForeignKey(c => c.IdCliente)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using AutoShopBD.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace AutoShopBD
 {
     public class Program
@@ -6,16 +9,16 @@ namespace AutoShopBD
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.Services.AddDbContext<AutoShopBDContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("AutoShopConnection")));
+
             builder.Services.AddRazorPages();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
