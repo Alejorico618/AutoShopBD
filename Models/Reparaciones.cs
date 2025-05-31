d78d0c832890bb97c7b6509d090869ea85001b97
Alejorico618 <d.alejandrorico2@gmail.com>
2025-05-31 11:43:43 -0500

    creacionn de Reparaciones y Vehiculos .cs

--- Models/Reparaciones.cs
+++ Models/Reparaciones.cs
@@ -1,6 +1,24 @@
-﻿namespace AutoShopBD.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AutoShopBD.Models
 {
     public class Reparaciones
     {
+        [Key]
+        public int IdReparacion { get; set; }
+
+        public string Descripcion { get; set; }
+
+        [ForeignKey("Vehiculos")]
+        public int IdVehiculo { get; set; }
+        public Vehiculos Vehiculo { get; set; }
+
+        public DateTime FechaInicio { get; set; }
+
+        public DateTime FechaFin { get; set; }
+
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal CostoEstimado { get; set; }
     }
 }
